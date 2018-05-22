b47989bc377105cdf662114863760ad3418fbc53
julien <julien.audet.1@ens.etsmtl.ca>
2018-05-22 17:51:42 -0400

    HSVToRGB et RGBToHSV foonctionne

--- src/view/HSVColorMediator.java
+++ src/view/HSVColorMediator.java
@@ -9,10 +9,12 @@ import java.awt.image.BufferedImage;
 
 public class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 
+    private static final int HUE = 0;
+    private static final int SATURATION = 1;
+    private static final int VALUE = 2;
     ColorSlider hueCS;
     ColorSlider saturationCS;
     ColorSlider valueCS;
-    float hsv[];
     int red;
     int green;
     int blue;
@@ -35,23 +37,113 @@ public class HSVColorMediator extends Object implements SliderObserver, Observer
         this.result = result;
         result.addObserver(this);
 
+        float[] hsv = RGBToHSV(red,green,blue);
+
+        this.hue = (int) hsv[HUE];
+        this.saturation = (int) hsv[SATURATION];
+        this.value = (int) hsv[VALUE];
+
+        int[] rgb = HSVToRGB(hue, saturation, value);
+
+        System.out.println("hue: " + hsv[0]);
+        System.out.println("saturation: " + hsv[1]);
+        System.out.println("value: " + hsv[2]);
+
+        System.out.println("r: " + rgb[0]);
+        System.out.println("g: " + rgb[1]);
+        System.out.println("b: " + rgb[2]);
+
+
         hueImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
         saturationImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
         valueImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
 
-        computeHueImage(red, green, blue);
-        computeSaturationImage(red, green, blue);
-        computeValueImage(red, green, blue);
+        computeHueImage(hue, saturation, value);
+        computeSaturationImage(hue, saturation, value);
+        computeValueImage(hue, saturation, value);
     }
 
-    public void RGBToHSV(int r, int g, int b){
+    /**
+     * Converts RGB to HSV
+     * @param r red pixel
+     * @param g green pixel
+     * @param b blue pixel
+     */
+    public float[] RGBToHSV(int r, int g, int b){
 
-        hsv = new float[3];
+        float[] hsv = new float[3];
         Color.RGBtoHSB(r,g,b,hsv);
-        hue = (int)hsv[0];
-        saturation = (int)hsv[1];
-        value = (int)hsv[2];
+        hsv[0] = hsv[0]*360;
+        hsv[1] = hsv[1]*100;
+        hsv[2] = hsv[2]*100;
+        return hsv;
+    }
 
+    /**
+     * Converts HSV to RGB
+     * Source : https://stackoverflow.com/questions/7896280/converting-from-hsv-hsb-in-java-to-rgb-without-using-java-awt-color-disallowe?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+     * @param h Hue
+     * @param s Saturation
+     * @param v Value
+     */
+    public int[] HSVToRGB(float h, float s, float v){
+
+        int[] rgbArray = new int[3];
+
+        h = h/360;
+        s = s/100;
+        v = v/100;
+
+        float r = 0,g = 0,b = 0;
+
+        int _h = (int) (h*6);
+        float f = h * 6 - _h;
+        float p = v * (1 - s);
+        float q = v * (1- f * s);
+        float t = v * (1- (1-f) * s );
+
+        if(_h == 0){
+            r = v;
+            g = t;
+            b = p;
+        }
+
+        else if(_h == 1){
+            r = q;
+            g = v;
+            b = t;
+        }
+
+        else if(_h == 2){
+            r = p;
+            g = v;
+            b = t;
+        }
+
+        else if(_h == 3){
+            r = p;
+            g = q;
+            b = v;
+        }
+
+        else if(_h == 4){
+            r = t;
+            g = p;
+            b = v;
+        }
+
+        else if(_h <= 6){
+            r = v;
+            g = p;
+            b = q;
+        }
+
+        rgbArray[0] = (int) r *255;
+        rgbArray[1] = (int) g *255;
+        rgbArray[2] = (int) b *255;
+
+
+        return rgbArray;
     }
 
 
@@ -106,6 +198,11 @@ public class HSVColorMediator extends Object implements SliderObserver, Observer
         green = result.getPixel().getGreen();
         blue = result.getPixel().getBlue();
 
+        float[] hsvColor = RGBToHSV(red, green, blue);
+
+        hue = (int) hsvColor[HUE];
+        saturation = (int) hsvColor[SATURATION];
+        value = (int) hsvColor[VALUE];
 
         hueCS.setValue(hue);
         saturationCS.setValue(saturation);
@@ -124,11 +221,18 @@ public class HSVColorMediator extends Object implements SliderObserver, Observer
         // harder to understand.
     }
 
-    public void computeHueImage(int red, int green, int blue) {
+    public void computeHueImage(int hue, int saturation, int value) {
+
         Pixel p = new Pixel(red, green, blue, 255);
-        RGBToHSV(red,green,blue);
+
+        float[] hsv = RGBToHSV(red,green,blue);
+
         for (int i = 0; i<imagesWidth; ++i) {
-            p.setRed((int)(((double)i / (double)imagesWidth)*360.0));
+            hue = ((int)(((double)i / (double)imagesWidth)*255.0));
+            HSVToRGB(red,green,blue);
+            p.setRed(red);
+            p.setGreen(green);
+            p.setBlue(blue);
             int rgb = p.getARGB();
             for (int j = 0; j<imagesHeight; ++j) {
                 hueImage.setRGB(i, j, rgb);
@@ -139,10 +243,11 @@ public class HSVColorMediator extends Object implements SliderObserver, Observer
         }
     }
 
-    public void computeSaturationImage(int red, int green, int blue) {
+    public void computeSaturationImage(int hue, int saturation, int value) {
         Pixel p = new Pixel(red, green, blue, 255);
+        RGBToHSV(red,green,blue);
         for (int i = 0; i<imagesWidth; ++i) {
-            p.setGreen((int)(((double)i / (double)imagesWidth)*255.0));
+            saturation = ((int)(((double)i / (double)imagesWidth)*255.0));
             int rgb = p.getARGB();
             for (int j = 0; j<imagesHeight; ++j) {
                 saturationImage.setRGB(i, j, rgb);
@@ -153,10 +258,11 @@ public class HSVColorMediator extends Object implements SliderObserver, Observer
         }
     }
 
-    public void computeValueImage(int red, int green, int blue) {
+    public void computeValueImage(int hue, int saturation, int value) {
         Pixel p = new Pixel(red, green, blue, 255);
+        RGBToHSV(red,green,blue);
         for (int i = 0; i<imagesWidth; ++i) {
-            p.setBlue((int)(((double)i / (double)imagesWidth)*255.0));
+            value = ((int)(((double)i / (double)imagesWidth)*255.0));
             int rgb = p.getARGB();
             for (int j = 0; j<imagesHeight; ++j) {
                 valueImage.setRGB(i, j, rgb);
